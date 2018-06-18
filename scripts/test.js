d5ed57a6c52cb628d7332d4fbffc63692fa49245
Tim Allen <tim@noblesamurai.com>
2018-06-18 14:09:56 +1000

    Rename package.

--- scripts/test.js
+++ scripts/test.js
@@ -1,5 +1,5 @@
 var assert = require('assert'),
-  php = require( 'php' );
+  php = require( '..' );
 
 assert.equal( php.md5crypt('rasmuslerdorf', '$1$rasmusle$'), '$1$rasmusle$rISCgZzpwk3UhDidwXvin0' );
 
